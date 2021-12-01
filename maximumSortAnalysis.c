b8cad910bab54c6a91ee99491e52ff6f7cab075a
CaptainGimmy <81749327+Captaingimmy@users.noreply.github.com>
2021-12-01 21:27:41 +0100

    Create maximumSortAnalysis.c

---
+++ maximumSortAnalysis.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main()
+{
+    int elenco[10000],k,pos_max,max,appo,j;
+    
+    for(k = 0;k<10000;k++)
+    {
+         elenco[k] = rand();
+    }
+    clock_t start = clock();
+    int ultimo = 9999;
+    while(ultimo > 0)
+    {
+        max = elenco[0];
+        pos_max = 0;
+        for(k=1;k<=ultimo;k++)
+        {
+            if(elenco[k] > max)
+            {
+                pos_max = k;
+                max = elenco[k];
+            }
+        }
+        appo = elenco[ultimo];
+        elenco[ultimo] = max;
+        elenco[pos_max] = appo;
+        ultimo--;
+    }
+    clock_t end = clock();
+    printf("Time elapsed: %f second \n", (double)(end-start) / CLOCKS_PER_SEC);
+    for(k = 0;k<10000;k++)
+    {
+        printf("%d,", elenco[k]);
+    }
+    
+}
