f756091b713ac30f6297bcb17eeb9232335144d6
CaptainGimmy <81749327+Captaingimmy@users.noreply.github.com>
2021-12-01 22:29:18 +0100

    Update minimumSortAnalysis.c

--- minimumSortAnalysis.c
+++ minimumSortAnalysis.c
@@ -4,33 +4,35 @@
 
 int main()
 {
-    int elenco[10000],k,pos_max,max,appo,j;
+    int elenco[10000],k,pos_min,min,appo;
     
     for(k = 0;k<10000;k++)
     {
         elenco[k] = rand();
     }
-    int ultimo = 9999;
+    
+    int ultimo = 0;
     clock_t start = clock();
-    while(ultimo > 0)
+    while(ultimo < 10000)
     {
-        max = elenco[0];
-        pos_max = 0;
-        for(k=1;k<=ultimo;k++)
+        min = elenco[9999];
+        pos_min = 9999;
+        for(k=ultimo;k<10000;k++)
         {
-            if(elenco[k] > max)
+            if(elenco[k] < min)
             {
-                pos_max = k;
-                max = elenco[k];
+                min = elenco[k];
+                pos_min = k;
             }
         }
         appo = elenco[ultimo];
-        elenco[ultimo] = max;
-        elenco[pos_max] = appo;
-        ultimo--;
+        elenco[ultimo] = min;
+        elenco[pos_min] = appo;
+        ultimo++;
     }
     clock_t end = clock();
     printf("Time elapsed: %f second \n", (double)(end-start) / CLOCKS_PER_SEC);
+    
     for(k = 0;k<10000;k++)
     {
         printf("%d,", elenco[k]);
